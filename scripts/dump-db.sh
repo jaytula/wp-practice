3370c5eae68cbcc1c31fad4b5833e4ba3b7848a9
Jay T <jaytula@gmail.com>
2021-10-04 13:24:24 -0700

    Require DBDUMP_FOLDER in dump and restore scripts

--- scripts/dump-db.sh
+++ scripts/dump-db.sh
@@ -1,5 +1,10 @@
 source .env
 
-TARGET=./backups/fictu-$(git rev-parse --short HEAD).sql
+if [ -z "$DBDUMP_FOLDER" ]; then
+  echo 'Environment variable DBDUMP_FOLDER is not set'
+  exit 1
+fi
+
+TARGET=$DBDUMP_FOLDER/fictu-$(git rev-parse --short HEAD).sql
 docker exec -i wp-practice_db_1 mysqldump -u $MYSQL_USER -p$MYSQL_PASS $MYSQL_DBNAME > $TARGET
 gzip $TARGET
--- scripts/restore-db.sh
+++ scripts/restore-db.sh
@@ -1,5 +1,10 @@
 source .env
 
-SOURCE=./backups/fictu-$(git rev-parse --short HEAD).sql.gz
+if [ -z "$DBDUMP_FOLDER" ]; then
+  echo 'Environment variable DBDUMP_FOLDER is not set'
+  exit 1
+fi
+
+SOURCE=$DBDUMP_FOLDER/fictu-$(git rev-parse --short HEAD).sql.gz
 
 zcat < $SOURCE | docker exec -i wp-practice_db_1 mysql -u $MYSQL_USER -p$MYSQL_PASS $MYSQL_DBNAME
