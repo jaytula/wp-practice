e7226cdd534f090cd0bf27bacec0bec77a621be5
Jay T <jaytula@gmail.com>
2021-10-04 21:29:25 -0700

    9. The Famous "Loop" in Wordpress

--- src/wp-content/themes/fictional-university-theme/page.php
+++ src/wp-content/themes/fictional-university-theme/page.php
@@ -1,6 +1,6 @@
 <?php while(have_posts()): ?>
     <?php the_post(); ?>
-    <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+    <h1>This is a page, not a post</h1>
+    <h2><?php the_title(); ?></h2>
     <?php the_content() ?>
-    <hr>
 <?php endwhile; ?>
