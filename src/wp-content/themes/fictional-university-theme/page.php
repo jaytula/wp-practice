50f3aa66a121637ce1865d1515233e74cfccff4e
Jay T <jaytula@gmail.com>
2021-10-04 22:43:39 -0700

    10. Header & Footer

--- src/wp-content/themes/fictional-university-theme/page.php
+++ src/wp-content/themes/fictional-university-theme/page.php
@@ -1,6 +1,10 @@
+<?php get_header(); ?>
+
 <?php while(have_posts()): ?>
     <?php the_post(); ?>
     <h1>This is a page, not a post</h1>
     <h2><?php the_title(); ?></h2>
     <?php the_content() ?>
 <?php endwhile; ?>
+
+<?php get_footer(); ?>
