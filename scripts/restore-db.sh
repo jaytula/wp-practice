2d59bb86344e8fb2480780693a429cd0b0071d5d
Jay T <jaytula@gmail.com>
2021-10-04 17:17:22 -0700

    Fall back to existing db dump

--- scripts/restore-db.sh
+++ scripts/restore-db.sh
@@ -5,6 +5,18 @@ if [ -z "$DBDUMP_FOLDER" ]; then
   exit 1
 fi
 
-SOURCE=$DBDUMP_FOLDER/fictu-$(git rev-parse --short HEAD).sql.gz
+REVISIONS=$(git rev-list HEAD | cut -c 1-7)
+REVISION=
+
+for revision in $REVISIONS; do
+  if [ -f "$DBDUMP_FOLDER/fictu-$revision.sql.gz" ]; then
+    REVISION=$revision
+    break
+  fi
+done
+
+echo "Restoring $revision"
+
+SOURCE=$DBDUMP_FOLDER/fictu-$revision.sql.gz
 
 zcat < $SOURCE | docker exec -i wp-practice_db_1 mysql -u $MYSQL_USER -p$MYSQL_PASS $MYSQL_DBNAME
