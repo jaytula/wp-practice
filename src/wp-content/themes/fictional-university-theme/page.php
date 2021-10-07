78bbedcc0c700c5ffbf1d42d39adb31af0da5444
Jay T <jaytula@gmail.com>
2021-10-07 12:53:40 -0700

    15. Parent & Children Pages

--- src/wp-content/themes/fictional-university-theme/page.php
+++ src/wp-content/themes/fictional-university-theme/page.php
@@ -13,16 +13,19 @@
     </div>
 
     <div class="container container--narrow page-section">
+      <?php $parent_id = wp_get_post_parent_id(get_the_ID()); ?>
+      <?php if($parent_id): ?>
       <div class="metabox metabox--position-up metabox--with-home-link">
         <p>
-          <a class="metabox__blog-home-link" href="#"><i class="fa fa-home" aria-hidden="true"></i> Back to About Us</a> <span class="metabox__main">Our History</span>
+          <a class="metabox__blog-home-link" href="<?php echo get_permalink($parent_id)?>"><i class="fa fa-home" aria-hidden="true"></i> Back to <?php echo get_the_title($parent_id) ?></a> <span class="metabox__main"><?php the_title() ?></span>
         </p>
       </div>
+      <?php endif; ?>
 
       <!-- <div class="page-links">
         <h2 class="page-links__title"><a href="#">About Us</a></h2>
         <ul class="min-list">
-          <li class="current_page_item"><a href="#">Our History</a></li>
+          <li class="current_page_item"><a href="#">Our Hiaastory</a></li>
           <li><a href="#">Our Goals</a></li>
         </ul>
       </div> -->
