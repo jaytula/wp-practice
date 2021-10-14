e80ca05cb3650802f7981edc5aa4d320d645b7c1
Jay T <jaytula@gmail.com>
2021-10-13 20:45:21 -0700

    29. Quick Timeout: Misc Updates

--- src/wp-content/themes/fictional-university-theme/front-page.php
+++ src/wp-content/themes/fictional-university-theme/front-page.php
@@ -28,13 +28,14 @@
                 </a>
                 <div class="event-summary__content">
                     <h5 class="event-summary__title headline headline--tiny"><a href="<?php the_permalink() ?>"><?php the_title() ?></a></h5>
-                    <p><?php echo wp_trim_words(get_the_content(), 18) ?> <a href="<?php the_permalink() ?>" class="nu gray">Learn more</a></p>
+                    <?php $excerpt = has_excerpt() ? get_the_excerpt() : wp_trim_words(get_the_content(), 18); ?>
+                    <p><?php echo $excerpt ?> <a href="<?php the_permalink() ?>" class="nu gray">Learn more</a></p>
                 </div>
             </div>
             <?php endwhile; ?>
             <?php wp_reset_postdata(); ?>
 
-            <p class="t-center no-margin"><a href="#" class="btn btn--blue">View All Events</a></p>
+            <p class="t-center no-margin"><a href="<?= get_post_type_archive_link('event') ?>" class="btn btn--blue">View All Events</a></p>
         </div>
     </div>
     <div class="full-width-split__two">
@@ -54,7 +55,8 @@
                 </a>
                 <div class="event-summary__content">
                     <h5 class="event-summary__title headline headline--tiny"><a href="<?php the_permalink()?>"><?php the_title() ?></a></h5>
-                    <p><?php echo wp_trim_words(get_the_content(), 18) ?> <a href="<?php the_permalink() ?>" class="nu gray">Read more</a></p>
+                    <?php $excerpt = has_excerpt() ? get_the_excerpt() : wp_trim_words(get_the_content(), 18); ?>
+                    <p><?php echo $excerpt ?> <a href="<?php the_permalink() ?>" class="nu gray">Read more</a></p>
                 </div>
             </div>
             <?php endwhile; ?>
