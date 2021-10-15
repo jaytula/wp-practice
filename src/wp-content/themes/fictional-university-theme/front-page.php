8b195aa9845a7a18f3e3d528db542ab02b95b502
Jay T <jaytula@gmail.com>
2021-10-15 16:58:47 -0700

    30. Custom Fields

--- src/wp-content/themes/fictional-university-theme/front-page.php
+++ src/wp-content/themes/fictional-university-theme/front-page.php
@@ -22,9 +22,12 @@
             <?php while($events_query->have_posts()): ?>
             <?php $events_query->the_post(); ?>
             <div class="event-summary">
+                <?php
+                $event_date = new DateTime(get_field('event_date'));
+                ?>
                 <a class="event-summary__date t-center" href="#">
-                    <span class="event-summary__month"><?php the_time('M') ?></span>
-                    <span class="event-summary__day"><?php the_time('d') ?></span>
+                    <span class="event-summary__month"><?= $event_date->format('M')  ?></span>
+                    <span class="event-summary__day"><?= $event_date->format('d') ?></span>
                 </a>
                 <div class="event-summary__content">
                     <h5 class="event-summary__title headline headline--tiny"><a href="<?php the_permalink() ?>"><?php the_title() ?></a></h5>
