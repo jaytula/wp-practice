c6dfcb1c341565ad264ef142c5726e5e917c93de
Jay T <jaytula@gmail.com>
2021-10-15 18:07:45 -0700

    33. Manipulating Default URL Based Queries

--- src/wp-content/themes/fictional-university-theme/archive-event.php
+++ src/wp-content/themes/fictional-university-theme/archive-event.php
@@ -15,10 +15,11 @@
 <div class="container container--narrow page-section">
     <?php while (have_posts()) : ?>
         <?php the_post(); ?>
+        <?php $event_date = new DateTime(get_field('event_date')); ?>
         <div class="event-summary">
             <a class="event-summary__date t-center" href="#">
-                <span class="event-summary__month"><?php the_time('M') ?></span>
-                <span class="event-summary__day"><?php the_time('d') ?></span>
+                <span class="event-summary__month"><?= $event_date->format('M') ?></span>
+                <span class="event-summary__day"><?= $event_date->format('d') ?></span>
             </a>
             <div class="event-summary__content">
                 <h5 class="event-summary__title headline headline--tiny"><a href="<?php the_permalink() ?>"><?php the_title() ?></a></h5>
