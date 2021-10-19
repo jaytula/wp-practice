01a31d9655079114ba98099bca2f309e2865c04d
Jay T <jaytula@gmail.com>
2021-10-19 00:08:01 -0700

    61. Waiting / Loading Spinner Icon

--- src/wp-content/themes/fictional-university-theme/src/modules/Search.js
+++ src/wp-content/themes/fictional-university-theme/src/modules/Search.js
@@ -2,6 +2,10 @@ class Search {
   // 1. describe and create/initial our object
   constructor() {
     /**
+     * @type {HTMLDivElement}
+     */
+    this.resultsDiv = document.getElementById("search-overlay__results");
+    /**
      * @type {HTMLSpanElement}
      */
     this.openButton = document.querySelector(".search-trigger");
@@ -17,43 +21,66 @@ class Search {
     /**
      * @type {HTMLInputElement}
      */
-    this.searchField = document.getElementById('search-term');
+    this.searchField = document.getElementById("search-term");
 
     this.events();
     this.isOverlayOpen = false;
     this.typingTimer;
+    this.previousValue;
+    this.isSpinnerVisible = false;
   }
 
   // 2. events
   events() {
     this.openButton.addEventListener("click", this.openOverlay.bind(this));
-    this.closeButton.addEventListener("click", this.closeOverlay.bind(this))
-    document.addEventListener('keydown', this.keyPressDispatcher.bind(this));
-    this.searchField.addEventListener('keydown', this.typingLogic.bind(this))
+    this.closeButton.addEventListener("click", this.closeOverlay.bind(this));
+    document.addEventListener("keydown", this.keyPressDispatcher.bind(this));
+    this.searchField.addEventListener("keyup", this.typingLogic.bind(this));
   }
 
   // 3. methods (function, action...)
 
   /**
-   * 
-   * @param {KeyboardEvent} event 
+   *
+   * @param {KeyboardEvent} event
    */
   typingLogic(event) {
-    if(typeof this.typingTimer === 'number') clearTimeout(this.typingTimer)
-    this.typingTimer = setTimeout(() => {
-      console.log(event.key)
-    }, 2000)
+    if (this.searchField.value === "") {
+      this.resultsDiv.innerHTML = "";
+      this.isSpinnerVisible = false;
+      this.previousValue = "";
+      return;
+    }
+
+    if (this.searchField.value === this.previousValue) return;
+
+    if (typeof this.typingTimer === "number") clearTimeout(this.typingTimer);
+    if (!this.isSpinnerVisible) {
+      this.resultsDiv.innerHTML = '<div class="spinner-loader"></div>';
+      this.isSpinnerVisible = true;
+    }
+    this.typingTimer = setTimeout(this.getResults.bind(this), 2000);
+    this.previousValue = this.searchField.value;
+  }
+
+  getResults() {
+    this.isSpinnerVisible = false;
+    this.resultsDiv.innerHTML = "Imagine real search results here...";
   }
 
   /**
-   * 
-   * @param {KeyboardEvent} event 
+   *
+   * @param {KeyboardEvent} event
    */
   keyPressDispatcher(event) {
-    if(event.code === 'KeyS' && !this.isOverlayOpen) {
+    if (
+      event.code === "KeyS" &&
+      !this.isOverlayOpen &&
+      !document.querySelector("input:focus, textarea:focus")
+    ) {
       this.openOverlay();
     }
-    if(event.code === 'Escape' && this.isOverlayOpen) {
+    if (event.code === "Escape" && this.isOverlayOpen) {
       this.closeOverlay();
     }
   }
@@ -63,16 +90,16 @@ class Search {
    */
   openOverlay() {
     this.searchOverlay.classList.add("search-overlay--active");
-    document.body.classList.add('body-no-scroll');
-    console.log('open ran');
+    document.body.classList.add("body-no-scroll");
+    console.log("open ran");
     this.isOverlayOpen = true;
   }
 
   closeOverlay() {
     this.searchOverlay.classList.remove("search-overlay--active");
-    document.body.classList.remove('body-no-scroll');
-    console.log('close ran');
-    this.isOverlayOpen = false
+    document.body.classList.remove("body-no-scroll");
+    console.log("close ran");
+    this.isOverlayOpen = false;
   }
 }
 
