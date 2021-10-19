1e9357002cfb7bd8e67e4aa52d55e84f31c269b1
Jay T <jaytula@gmail.com>
2021-10-18 23:41:03 -0700

    60. Managing Time in JavaScript

--- src/wp-content/themes/fictional-university-theme/src/modules/Search.js
+++ src/wp-content/themes/fictional-university-theme/src/modules/Search.js
@@ -14,8 +14,14 @@ class Search {
      */
     this.searchOverlay = document.querySelector(".search-overlay");
 
+    /**
+     * @type {HTMLInputElement}
+     */
+    this.searchField = document.getElementById('search-term');
+
     this.events();
     this.isOverlayOpen = false;
+    this.typingTimer;
   }
 
   // 2. events
@@ -23,9 +29,21 @@ class Search {
     this.openButton.addEventListener("click", this.openOverlay.bind(this));
     this.closeButton.addEventListener("click", this.closeOverlay.bind(this))
     document.addEventListener('keydown', this.keyPressDispatcher.bind(this));
+    this.searchField.addEventListener('keydown', this.typingLogic.bind(this))
   }
 
   // 3. methods (function, action...)
+
+  /**
+   * 
+   * @param {KeyboardEvent} event 
+   */
+  typingLogic(event) {
+    if(typeof this.typingTimer === 'number') clearTimeout(this.typingTimer)
+    this.typingTimer = setTimeout(() => {
+      console.log(event.key)
+    }, 2000)
+  }
 
   /**
    * 
