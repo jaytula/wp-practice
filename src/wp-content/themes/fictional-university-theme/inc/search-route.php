fe19fad1ef2f90c26e314044ae1beaf404fd49d2
Jay T <jaytula@gmail.com>
2021-10-20 14:34:42 -0700

    75. Custom Layout & JSON based on Post Type

--- src/wp-content/themes/fictional-university-theme/inc/search-route.php
+++ src/wp-content/themes/fictional-university-theme/inc/search-route.php
@@ -1,44 +1,66 @@
 <?php
 
-function universityRegisterSearch() {
+function universityRegisterSearch()
+{
     register_rest_route('university/v1', 'search', array(
-        'methods' => WP_REST_Server::READABLE,       
+        'methods' => WP_REST_Server::READABLE,
         'callback' => 'universitySearchResults'
     ));
 }
 
-function universitySearchResults($data) {
+function universitySearchResults($data)
+{
     $mainQuery = new WP_Query(array(
-      'post_type' => array('post', 'page', 'professor', 'program', 'campus', 'event'),
-      's' => sanitize_text_field($data['term'])
+        'post_type' => array('post', 'page', 'professor', 'program', 'campus', 'event'),
+        's' => sanitize_text_field($data['term'])
     ));
 
     $results = array(
-      'generalInfo' => array(),
-      'professors' => array(),
-      'programs' => array(),
-      'events' => array(),
-      'campuses' => array()
+        'generalInfo' => array(),
+        'professors' => array(),
+        'programs' => array(),
+        'events' => array(),
+        'campuses' => array()
     );
 
-    while($mainQuery->have_posts()) {
-      $mainQuery->the_post();
+    while ($mainQuery->have_posts()) {
+        $mainQuery->the_post();
 
-      $resultsKey = 'generalInfo';
-      switch(get_post_type()) {
-        case 'professor': $resultsKey = 'professors'; break;
-        case 'program': $resultsKey = 'programs'; break;
-        case 'event': $resultsKey = 'events'; break;
-        case 'campus': $resultsKey = 'campuses'; break;
+        $resultsKey = 'generalInfo';
+        switch (get_post_type()) {
+            case 'professor':
+                $resultsKey = 'professors';
+                break;
+            case 'program':
+                $resultsKey = 'programs';
+                break;
+            case 'event':
+                $resultsKey = 'events';
+                break;
+            case 'campus':
+                $resultsKey = 'campuses';
+                break;
+        }
 
-      }
+        $current = array(
+            'title' => get_the_title(),
+            'permalink' => get_the_permalink(),
+            'postType' => get_post_type(),
+            'authorName' => get_the_author(),
+        );
 
-      array_push($results[$resultsKey], array(
-          'title' => get_the_title(),
-          'permalink' => get_the_permalink(),
-          'postType' => get_post_type(),
-          'authorName' => get_the_author(),
-      ));
+        if(get_post_type() == 'professor') {
+            $current['image'] = get_the_post_thumbnail_url(0, 'professorLandscape');
+        }
+
+        if(get_post_type() == 'event') {
+            $event_date = new DateTime(get_field('event_date'));
+
+            $current['month'] = $event_date->format('M');
+            $current['day'] = $event_date->format('d');
+            $current['description'] = has_excerpt() ? get_the_excerpt() : wp_trim_words(get_the_content(), 18);
+        }
+        array_push($results[$resultsKey], $current);
     }
     return $results;
 }
