28fe5e24c02a9cbfc19ef62be1971a495ed2f760
Jay T <jaytula@gmail.com>
2021-11-06 23:00:22 -0700

    94. Note Permissions and Security Part 1

--- src/wp-content/themes/fictional-university-theme/functions.php
+++ src/wp-content/themes/fictional-university-theme/functions.php
@@ -5,7 +5,7 @@ require get_theme_file_path('inc/search-route.php');
 function university_custom_rest()
 {
   register_rest_field('post', 'authorName', array(
-    'get_callback' => function() {
+    'get_callback' => function () {
       return get_the_author();
     }
   ));
@@ -13,18 +13,18 @@ function university_custom_rest()
 
 add_action('rest_api_init', 'university_custom_rest');
 
-function pageBanner($args=NULL)
-{ 
+function pageBanner($args = NULL)
+{
   $title = $args['title'] ?? get_the_title();
   $subtitle = $args['subtitle'] ?? get_field('page_banner_subtitle');
   $photo_url = $args['photo'] ?? get_theme_file_uri('/images/ocean.jpg');
-  if(!$args['photo'] && !is_archive() && !is_home()) {
+  if (!$args['photo'] && !is_archive() && !is_home()) {
     $page_banner_image = get_field('page_banner_image_background');
-    if($page_banner_image) {
+    if ($page_banner_image) {
       $photo_url = $page_banner_image['sizes']['pageBanner'];
     }
   }
-  ?>
+?>
   <div class="page-banner">
     <?php $page_banner_image = get_field('page_banner_image_background'); ?>
     <div class="page-banner__bg-image" style="background-image: url(<?= $photo_url ?>)"></div>
@@ -96,7 +96,8 @@ function university_adjust_queries($query)
 }
 add_action('pre_get_posts', 'university_adjust_queries');
 
-function universityMapKey($api) {
+function universityMapKey($api)
+{
   $api['key'] = getenv('GOOGLE_MAPS_API_KEY');
   return $api;
 }
@@ -107,10 +108,11 @@ add_filter('acf/fields/google_map/api', 'universityMapKey');
 
 add_action('admin_init', 'redirectSubsToFrontend');
 
-function redirectSubsToFrontend() {
+function redirectSubsToFrontend()
+{
   $ourCurrentUser = wp_get_current_user();
 
-  if(count($ourCurrentUser->roles) == 1 AND $ourCurrentUser->roles[0] == 'subscriber') {
+  if (count($ourCurrentUser->roles) == 1 and $ourCurrentUser->roles[0] == 'subscriber') {
     wp_redirect(site_url('/'));
     exit;
   }
@@ -118,10 +120,11 @@ function redirectSubsToFrontend() {
 
 add_action('wp_loaded', 'noSubsAdminBar');
 
-function noSubsAdminBar() {
+function noSubsAdminBar()
+{
   $ourCurrentUser = wp_get_current_user();
 
-  if(count($ourCurrentUser->roles) == 1 AND $ourCurrentUser->roles[0] == 'subscriber') {
+  if (count($ourCurrentUser->roles) == 1 and $ourCurrentUser->roles[0] == 'subscriber') {
     show_admin_bar(false);
   }
 }
@@ -129,13 +132,15 @@ function noSubsAdminBar() {
 // Customize Login Screen
 add_filter('login_headerurl', 'ourHeaderUrl');
 
-function ourHeaderUrl() {
+function ourHeaderUrl()
+{
   return esc_url(site_url('/'));
 }
 
 add_action('login_enqueue_scripts', 'ourLoginCSS');
 
-function ourLoginCSS() {
+function ourLoginCSS()
+{
   wp_enqueue_style('custom-google-fonts', '//fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i');
   wp_enqueue_style('font-awesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');
   wp_enqueue_style('university_main_styles', get_theme_file_uri('/build/style-index.css'));
@@ -144,6 +149,18 @@ function ourLoginCSS() {
 
 add_filter('login_headertitle', 'ourLoginTitle');
 
-function ourLoginTitle() {
+function ourLoginTitle()
+{
   return get_bloginfo('name');
+}
+
+// Force note posts to be private
+add_filter('wp_insert_post_data', 'makeNotePrivate');
+
+function makeNotePrivate($data)
+{
+  if ($data['post_type'] === 'note' && $data['post_status'] !== 'trash') {
+    $data['post_status'] = 'private';
+  }
+  return $data;
 }
