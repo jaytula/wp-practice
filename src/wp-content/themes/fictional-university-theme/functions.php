6d32dae33069462e99b29dbb5e9d8124a312a96c
Jay T <jaytula@gmail.com>
2021-11-21 19:37:39 -0800

    95. Note Permissions and Security (Part 2)

--- src/wp-content/themes/fictional-university-theme/functions.php
+++ src/wp-content/themes/fictional-university-theme/functions.php
@@ -159,6 +159,11 @@ add_filter('wp_insert_post_data', 'makeNotePrivate');
 
 function makeNotePrivate($data)
 {
+  if($data['post_type'] == 'note') {
+    $data['post_content'] = sanitize_textarea_field($data['post_content']);
+    $data['post_title'] = sanitize_text_field($data['post_title']);
+  }
+  
   if ($data['post_type'] === 'note' && $data['post_status'] !== 'trash') {
     $data['post_status'] = 'private';
   }
